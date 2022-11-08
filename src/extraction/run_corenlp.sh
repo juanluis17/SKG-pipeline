ba0419a751a5c48a55699c2746cb2b900a6e14ca
juanluis1702 <juanluis1702@gmail.com>
2022-11-08 10:27:20 -0600

    add arguments to corenlp_extractor

--- src/extraction/run_corenlp.sh
+++ src/extraction/run_corenlp.sh
@@ -1,17 +1,24 @@
+#!/bin/bash
+
+dataset_dump_dir=$1
+dygiepp_output_dump_dir=$2
+output_dir=$3
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate dygiepp
 pip install stanfordcorenlp networkx
 
-wget https://downloads.cs.stanford.edu/nlp/software/stanford-corenlp-4.5.1.zip
-unzip stanford-corenlp-latest.zip
-rm stanford-corenlp-latest.zip
+if  [ ! -e stanford-corenlp-4.5.1 ]; then
+  wget https://downloads.cs.stanford.edu/nlp/software/stanford-corenlp-4.5.1.zip
+  unzip stanford-corenlp-4.5.1.zip
+  rm stanford-corenlp-4.5.1.zip
+fi
 
 cd stanford-corenlp-4.5.0
 java -mx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9050 -timeout 15000 -threads 4 &
 corenlp_process_pid=$!
 cd ..
 echo $corenlp_process_pid
-python corenlp_extractor.py 4
+python corenlp_extractor.py 4 --dataset_dump_dir ${dataset_dump_dir} --dygiepp_output_dump_dir ${dygiepp_output_dump_dir} --output_dir ${output_dir}
 kill -9 $corenlp_process_pid
 
